bdaa60c40939dcfbcdae36e9f7091f23855f6de5
lllionlll <lam14.dev@gmail.com>
2022-10-25 16:20:36 +0700

    25/10/2022

---
+++ icon/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Icon(
+          Icons.apple, 
+          color: Colors.green,
+          size: 80,
+          shadows: [
+            Shadow(
+            color: Colors.amber, 
+            blurRadius: 100, 
+          )
+          ],
+        ),
+      ),
+    );
+  }
+}
